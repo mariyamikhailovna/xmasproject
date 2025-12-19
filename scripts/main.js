3485e22c1a72b555d3b1bf406fb1bfdb707fed05
Selene <itszemoff@gmail.com>
2025-12-19 09:01:09 +0800

    oopdate

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,23 @@
 const image = document.querySelector("img");
 const p = document.querySelector("p");
 const h6 = document.querySelector("h6");
+const title = document.getElementById("title");
+const currentTitle = title.textContent;
 
 p.addEventListener("click", () => {
   const pImage = image.getAttribute("src");
-  if (pImage === "images/silenceSuzuka.png") {
-    image.setAttribute("src", "images/summerSuzuka.png");
+  if(currentTitle == "Silence Suzuka"){
+  if (pImage === "../images/silenceSuzuka.png") {
+    image.setAttribute("src", "../images/summerSuzuka.png");
     h6.textContent = "Emerald Among the Waves";
   }
-  if (pImage === "images/summerSuzuka.png") {
-    image.setAttribute("src", "images/triesBien.png");
+  if (pImage === "../images/summerSuzuka.png") {
+    image.setAttribute("src", "../images/triesBien.png");
     h6.textContent = "Tracen Academy";
   }
-  if (pImage === "images/triesBien.png") {
-    image.setAttribute("src", "images/silenceSuzuka.png");
+  if (pImage === "../images/triesBien.png") {
+    image.setAttribute("src", "../images/silenceSuzuka.png");
     h6.textContent = "The Otherwordly Frontrunner";
   }
+}
 });
